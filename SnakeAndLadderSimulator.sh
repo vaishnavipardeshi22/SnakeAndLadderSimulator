9d2ef2a78fa1befa1f9d265e13b0736b7157a1fc
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-02-21 10:00:00 +0530

    ADDED : RANDOM() for roll die of player

--- SnakeAndLadderSimulator.sh
+++ SnakeAndLadderSimulator.sh
@@ -5,3 +5,6 @@ echo " ******************************* WELCOME TO SNAKE AND LADDER SIMULATION **
 #CONSTANTS
 NUMBER_OF_PLAYER=1
 START_POSITION=0
+
+#ROLL DIE FOR PLAYER
+rollDie=$(( 1 + RANDOM % 6 ))
